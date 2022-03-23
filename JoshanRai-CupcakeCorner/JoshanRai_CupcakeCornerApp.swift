ad8e83bc58a9af8e60ea64f70b5a19bfbe1ffb38
Joshan Rai <pradheon@gmail.com>
2022-03-23 14:52:36 -0700

    Initial Commit

---
+++ JoshanRai-CupcakeCorner/JoshanRai_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_CupcakeCornerApp.swift
+//  JoshanRai-CupcakeCorner
+//
+//  Created by Joshan Rai on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
